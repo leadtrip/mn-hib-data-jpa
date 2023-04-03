361248ef0a9dc8ac9721f7016bd3dab1a9dd3d83
leadtrip <woodm@objectcomputing.com>
2023-04-03 14:11:46 +0100

    Updates

--- src/main/java/example/micronaut/services/BookRepository.java
+++ src/main/java/example/micronaut/services/BookRepository.java
@@ -2,18 +2,25 @@ package example.micronaut.services;
 
 import example.micronaut.domain.Book;
 import example.micronaut.domain.Genre;
-import io.micronaut.data.annotation.Join;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.annotation.EntityGraph;
 import io.micronaut.data.repository.reactive.ReactorPageableRepository;
 import reactor.core.publisher.Mono;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 import java.util.UUID;
 
-@Join(value = "genres", type = Join.Type.LEFT_FETCH)
 @Repository
 public interface BookRepository extends ReactorPageableRepository<Book, UUID> {
 
+    @EntityGraph( attributePaths = {"genres"})
+    @NonNull
+    @Override
+    Mono<Book> findById(@NonNull @NotNull UUID id);
+
+    @EntityGraph( attributePaths = {"genres"})
     default Mono<Book> updateGenres(UUID id, Set<Genre> genres) {
         return findById(id).map( book -> {
                 book.setGenres(genres);
---
+++ src/test/java/micronaut/BookControllerTest.java
@@ -0,0 +1,170 @@
+package micronaut;
+
+import example.micronaut.commands.BookCreateCommand;
+import example.micronaut.commands.BookUpdateCommand;
+import example.micronaut.domain.Book;
+import example.micronaut.domain.Genre;
+import example.micronaut.services.GenreRepository;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BookControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Inject
+    GenreRepository genreRepository;
+
+    @Test
+    public void testBookCrudOperations() {
+        // add some genres
+        Set<Genre> genres = new HashSet<>();
+
+        // genre 1
+        HttpRequest<?> request = HttpRequest.POST("/genres", Collections.singletonMap("name", "Programming"));
+        HttpResponse<?> response = httpClient.toBlocking().exchange(request);
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+        UUID genreId = entityId(response, "/genres/");
+        request = HttpRequest.GET("/genres/" + genreId);
+        Genre genre = httpClient.toBlocking().retrieve(request, Genre.class);
+        genres.add(genre);
+        // =============================
+
+        // genre 2
+        request = HttpRequest.POST("/genres", Collections.singletonMap("name", "Database"));
+        response = httpClient.toBlocking().exchange(request);
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+        genreId = entityId(response, "/genres/");
+        request = HttpRequest.GET("/genres/" + genreId);
+        genre = httpClient.toBlocking().retrieve(request, Genre.class);
+        genres.add(genre);
+        // =============================
+
+        // genre 3
+        request = HttpRequest.POST("/genres", Collections.singletonMap("name", "JSON"));
+        response = httpClient.toBlocking().exchange(request);
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+        genreId = entityId(response, "/genres/");
+        request = HttpRequest.GET("/genres/" + genreId);
+        genre = httpClient.toBlocking().retrieve(request, Genre.class);
+        genres.add(genre);
+        // =============================
+
+        Long totalGenres = (long) genres.size();
+        assertEquals(3, totalGenres);
+
+        // add a book with 2 genres
+        BookCreateCommand bookCreateCommand = new BookCreateCommand(
+                "Head first Java",
+                genres.stream().map(Genre::getId).limit(2).collect(Collectors.toSet()));
+
+        request = HttpRequest.POST("/books", bookCreateCommand);
+        response = httpClient.toBlocking().exchange(request);
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+        // =============================
+
+        // Check the book and associated genres are correct
+        UUID bookId = entityId(response, "/books/");
+        request = HttpRequest.GET("/books/" + bookId);
+        Book fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
+        System.out.println(fetchedBook);
+        assertEquals( totalGenres , genreRepository.count().block() );
+        assertEquals( totalGenres - 1, fetchedBook.getGenres().size());
+        // =============================
+
+        // remove a genre from the book
+        BookUpdateCommand bookUpdateCommand = new BookUpdateCommand();
+        bookUpdateCommand.setId(fetchedBook.getId());
+        bookUpdateCommand.setName("Java is great");
+        bookUpdateCommand.setGenres(Set.of(genreId));
+
+        request = HttpRequest.PUT("/books", bookUpdateCommand);
+        httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
+
+        // Fetch the updated book and check genres updates
+        request = HttpRequest.GET("/books/" + bookId);
+        fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
+        System.out.println(fetchedBook);
+
+        assertEquals( totalGenres, genreRepository.count().block() );
+        assertEquals( totalGenres - 2, fetchedBook.getGenres().size());
+
+        // Update book again with all 3 genres
+        bookUpdateCommand.setName("Java is great");
+        bookUpdateCommand.setGenres(genres.stream().map(Genre::getId).collect(Collectors.toSet()));
+
+        request = HttpRequest.PUT("/books", bookUpdateCommand);
+        httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
+
+        // Fetch the updated book and check genres updates
+        request = HttpRequest.GET("/books/" + bookId);
+        fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
+        System.out.println(fetchedBook);
+
+        assertEquals( totalGenres , genreRepository.count().block() );
+        assertEquals( totalGenres, fetchedBook.getGenres().size());
+
+        // Remove all genres associated with book
+        bookUpdateCommand.setGenres(Set.of());        // you can do this or just omit, same result, no genre list = remove all
+
+        request = HttpRequest.PUT("/books", bookUpdateCommand);
+        httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
+
+        // Fetch the updated book and check genres removed
+        request = HttpRequest.GET("/books/" + bookId);
+        fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
+
+        assertEquals( totalGenres , genreRepository.count().block() );
+        assertNull( fetchedBook.getGenres() );
+
+        // Use PATCH to update genres
+        bookUpdateCommand.setGenres(genres.stream().map(Genre::getId).collect(Collectors.toSet()));
+
+        request = HttpRequest.PATCH("/books", bookUpdateCommand);
+        httpClient.toBlocking().exchange(request, Argument.of(Book.class), Argument.of(JsonError.class));
+
+        // Fetch the updated book and check genres
+        request = HttpRequest.GET("/books/" + bookId);
+        fetchedBook = httpClient.toBlocking().retrieve(request, Book.class);
+        System.out.println(fetchedBook);
+
+        assertEquals( totalGenres , genreRepository.count().block() );
+
+        // this is failing I think because @Transactional is needed on the BookRepository updateGenres method but then the update fails above with
+        // 'No reactive transaction management has been configured. Ensure you have correctly configured a reactive capable transaction manager'
+
+        //assertEquals( totalGenres, fetchedBook.getGenres().size());
+    }
+
+    protected UUID entityId(HttpResponse<?> response, String path) {
+        String value = response.header(HttpHeaders.LOCATION);
+        if (value == null) {
+            return null;
+        }
+        int index = value.indexOf(path);
+        if (index != -1) {
+            return UUID.fromString(value.substring(index + path.length()));
+        }
+        return null;
+    }
+}
